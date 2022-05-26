5f12bc20bb7727a01d3de0bee15f48129bcab972
RoosterQMonee <82356323+RoosterQMonee@users.noreply.github.com>
2022-05-26 11:04:39 -0400

    Add files via upload

---
+++ system/__init__.py
@@ -0,0 +1,816 @@
+#[ ! ] Generated language interpereter
+
+with open("generated.txt", 'w') as f: f.write(' ')
+output_file = open("generated.txt", 'a')
+def finalize(): output_file.close()
+
+
+
+def generate_speed_system (arg):
+    output_file.write(f"!speed@{arg}|")
+
+
+
+def generate_volume_system (arg):
+    output_file.write(f"!volume@{arg}|")
+
+
+
+def generate_stop_system (arg):
+    output_file.write(f"!stop@{arg}|")
+
+
+
+def generate_loopmany_system (arg):
+    output_file.write(f"!loopmany@{arg}|")
+
+
+
+def generate_loop_system (arg):
+    output_file.write(f"!loop@{arg}|")
+
+
+
+def generate_looptarget_system (arg):
+    output_file.write(f"!looptarget@{arg}|")
+
+
+
+def generate_cut_system (arg):
+    output_file.write(f"!cut@{arg}|")
+
+
+
+def generate_combine_system (arg):
+    output_file.write(f"!combine@{arg}|")
+
+
+
+def generate_jump_system (arg):
+    output_file.write(f"!jump@{arg}|")
+
+
+
+def generate_target_system (arg):
+    output_file.write(f"!target@{arg}|")
+
+
+
+def generate_flash_system (arg):
+    output_file.write(f"!flash@{arg}|")
+
+
+
+def generate_startpos_system (arg):
+    output_file.write(f"!startpos@{arg}|")
+
+
+
+def generate__pause (arg):
+    output_file.write(f"_pause@{arg}|")
+
+
+
+def generate_boom (arg):
+    output_file.write(f"boom@{arg}|")
+
+
+
+def generate_bruh (arg):
+    output_file.write(f"bruh@{arg}|")
+
+
+
+def generate_bong (arg):
+    output_file.write(f"bong@{arg}|")
+
+
+
+def generate_skull (arg):
+    output_file.write(f"skull@{arg}|")
+
+
+
+def generate_clap (arg):
+    output_file.write(f"clap@{arg}|")
+
+
+
+def generate_dog (arg):
+    output_file.write(f"dog@{arg}|")
+
+
+
+def generate_alien (arg):
+    output_file.write(f"alien@{arg}|")
+
+
+
+def generate_bell (arg):
+    output_file.write(f"bell@{arg}|")
+
+
+
+def generate_x (arg):
+    output_file.write(f"x@{arg}|")
+
+
+
+def generate_wind (arg):
+    output_file.write(f"wind@{arg}|")
+
+
+
+def generate_no (arg):
+    output_file.write(f"no@{arg}|")
+
+
+
+def generate_pizza (arg):
+    output_file.write(f"pizza@{arg}|")
+
+
+
+def generate_eight (arg):
+    output_file.write(f"eight@{arg}|")
+
+
+
+def generate_e (arg):
+    output_file.write(f"e@{arg}|")
+
+
+
+def generate_exclamation (arg):
+    output_file.write(f"exclamation@{arg}|")
+
+
+
+def generate_buzzer (arg):
+    output_file.write(f"buzzer@{arg}|")
+
+
+
+def generate_siren (arg):
+    output_file.write(f"siren@{arg}|")
+
+
+
+def generate_sunshine (arg):
+    output_file.write(f"sunshine@{arg}|")
+
+
+
+def generate_cellphone (arg):
+    output_file.write(f"cellphone@{arg}|")
+
+
+
+def generate_gun (arg):
+    output_file.write(f"gun@{arg}|")
+
+
+
+def generate_hitmarker (arg):
+    output_file.write(f"hitmarker@{arg}|")
+
+
+
+def generate_okhand (arg):
+    output_file.write(f"okhand@{arg}|")
+
+
+
+def generate_whatsapp (arg):
+    output_file.write(f"whatsapp@{arg}|")
+
+
+
+def generate_gnome (arg):
+    output_file.write(f"gnome@{arg}|")
+
+
+
+def generate_disc (arg):
+    output_file.write(f"disc@{arg}|")
+
+
+
+def generate_party (arg):
+    output_file.write(f"party@{arg}|")
+
+
+
+def generate_violin (arg):
+    output_file.write(f"violin@{arg}|")
+
+
+
+def generate_pan (arg):
+    output_file.write(f"pan@{arg}|")
+
+
+
+def generate_slip (arg):
+    output_file.write(f"slip@{arg}|")
+
+
+
+def generate_whipcrack (arg):
+    output_file.write(f"whipcrack@{arg}|")
+
+
+
+def generate_explosion (arg):
+    output_file.write(f"explosion@{arg}|")
+
+
+
+def generate_americano (arg):
+    output_file.write(f"americano@{arg}|")
+
+
+
+def generate_SLAM (arg):
+    output_file.write(f"SLAM@{arg}|")
+
+
+
+def generate_op (arg):
+    output_file.write(f"op@{arg}|")
+
+
+
+def generate_21 (arg):
+    output_file.write(f"21@{arg}|")
+
+
+
+def generate_stopposting (arg):
+    output_file.write(f"stopposting@{arg}|")
+
+
+
+def generate_morshu (arg):
+    output_file.write(f"morshu@{arg}|")
+
+
+
+def generate_granddad (arg):
+    output_file.write(f"granddad@{arg}|")
+
+
+
+def generate_hehehehaw (arg):
+    output_file.write(f"hehehehaw@{arg}|")
+
+
+
+def generate_yoda (arg):
+    output_file.write(f"yoda@{arg}|")
+
+
+
+def generate_necoarc (arg):
+    output_file.write(f"necoarc@{arg}|")
+
+
+
+def generate_subaluwa (arg):
+    output_file.write(f"subaluwa@{arg}|")
+
+
+
+def generate_oof (arg):
+    output_file.write(f"oof@{arg}|")
+
+
+
+def generate_smw_coin (arg):
+    output_file.write(f"smw_coin@{arg}|")
+
+
+
+def generate_smw_1up (arg):
+    output_file.write(f"smw_1up@{arg}|")
+
+
+
+def generate_smw_spinjump (arg):
+    output_file.write(f"smw_spinjump@{arg}|")
+
+
+
+def generate_smw_stomp2 (arg):
+    output_file.write(f"smw_stomp2@{arg}|")
+
+
+
+def generate_smw_kick (arg):
+    output_file.write(f"smw_kick@{arg}|")
+
+
+
+def generate_smw_stomp (arg):
+    output_file.write(f"smw_stomp@{arg}|")
+
+
+
+def generate_yahoo (arg):
+    output_file.write(f"yahoo@{arg}|")
+
+
+
+def generate_sm64_hurt (arg):
+    output_file.write(f"sm64_hurt@{arg}|")
+
+
+
+def generate_bup (arg):
+    output_file.write(f"bup@{arg}|")
+
+
+
+def generate_thwomp (arg):
+    output_file.write(f"thwomp@{arg}|")
+
+
+
+def generate_sm64_painting (arg):
+    output_file.write(f"sm64_painting@{arg}|")
+
+
+
+def generate_smm_scream (arg):
+    output_file.write(f"smm_scream@{arg}|")
+
+
+
+def generate_mariopaint_car (arg):
+    output_file.write(f"mariopaint_car@{arg}|")
+
+
+
+def generate_mariopaint_plane (arg):
+    output_file.write(f"mariopaint_plane@{arg}|")
+
+
+
+def generate_mariopaint_baby (arg):
+    output_file.write(f"mariopaint_baby@{arg}|")
+
+
+
+def generate_mariopaint_swan (arg):
+    output_file.write(f"mariopaint_swan@{arg}|")
+
+
+
+def generate_mariopaint_cat (arg):
+    output_file.write(f"mariopaint_cat@{arg}|")
+
+
+
+def generate_mariopaint_dog (arg):
+    output_file.write(f"mariopaint_dog@{arg}|")
+
+
+
+def generate_mariopaint_gameboy (arg):
+    output_file.write(f"mariopaint_gameboy@{arg}|")
+
+
+
+def generate_mariopaint_flower (arg):
+    output_file.write(f"mariopaint_flower@{arg}|")
+
+
+
+def generate_mariopaint_star (arg):
+    output_file.write(f"mariopaint_star@{arg}|")
+
+
+
+def generate_smw_yoshi (arg):
+    output_file.write(f"smw_yoshi@{arg}|")
+
+
+
+def generate_mariopaint_luigi (arg):
+    output_file.write(f"mariopaint_luigi@{arg}|")
+
+
+
+def generate_mariopaint_mario (arg):
+    output_file.write(f"mariopaint_mario@{arg}|")
+
+
+
+def generate_shaker (arg):
+    output_file.write(f"shaker@{arg}|")
+
+
+
+def generate_hammer (arg):
+    output_file.write(f"hammer@{arg}|")
+
+
+
+def generate_sidestick (arg):
+    output_file.write(f"sidestick@{arg}|")
+
+
+
+def generate_ride2 (arg):
+    output_file.write(f"ride2@{arg}|")
+
+
+
+def generate_buttonpop (arg):
+    output_file.write(f"buttonpop@{arg}|")
+
+
+
+def generate_skipshot (arg):
+    output_file.write(f"skipshot@{arg}|")
+
+
+
+def generate_otto_on (arg):
+    output_file.write(f"otto_on@{arg}|")
+
+
+
+def generate_otto_off (arg):
+    output_file.write(f"otto_off@{arg}|")
+
+
+
+def generate_otto_happy (arg):
+    output_file.write(f"otto_happy@{arg}|")
+
+
+
+def generate_otto_stress (arg):
+    output_file.write(f"otto_stress@{arg}|")
+
+
+
+def generate_adofaicymbal (arg):
+    output_file.write(f"adofaicymbal@{arg}|")
+
+
+
+def generate_midspin (arg):
+    output_file.write(f"midspin@{arg}|")
+
+
+
+def generate_adofaikick (arg):
+    output_file.write(f"adofaikick@{arg}|")
+
+
+
+def generate_samurai (arg):
+    output_file.write(f"samurai@{arg}|")
+
+
+
+def generate_rdmistake (arg):
+    output_file.write(f"rdmistake@{arg}|")
+
+
+
+def generate_tonk (arg):
+    output_file.write(f"tonk@{arg}|")
+
+
+
+def generate_preecho (arg):
+    output_file.write(f"preecho@{arg}|")
+
+
+
+def generate_tab_rooms (arg):
+    output_file.write(f"tab_rooms@{arg}|")
+
+
+
+def generate_tab_decorations (arg):
+    output_file.write(f"tab_decorations@{arg}|")
+
+
+
+def generate_tab_actions (arg):
+    output_file.write(f"tab_actions@{arg}|")
+
+
+
+def generate_tab_rows (arg):
+    output_file.write(f"tab_rows@{arg}|")
+
+
+
+def generate_tab_sounds (arg):
+    output_file.write(f"tab_sounds@{arg}|")
+
+
+
+def generate_cowbell (arg):
+    output_file.write(f"cowbell@{arg}|")
+
+
+
+def generate_karateman_throw (arg):
+    output_file.write(f"karateman_throw@{arg}|")
+
+
+
+def generate_karateman_offbeat (arg):
+    output_file.write(f"karateman_offbeat@{arg}|")
+
+
+
+def generate_karateman_hit (arg):
+    output_file.write(f"karateman_hit@{arg}|")
+
+
+
+def generate_karateman_bulb (arg):
+    output_file.write(f"karateman_bulb@{arg}|")
+
+
+
+def generate_ook (arg):
+    output_file.write(f"ook@{arg}|")
+
+
+
+def generate_choruskid (arg):
+    output_file.write(f"choruskid@{arg}|")
+
+
+
+def generate_builttoscale (arg):
+    output_file.write(f"builttoscale@{arg}|")
+
+
+
+def generate_perfectfail (arg):
+    output_file.write(f"perfectfail@{arg}|")
+
+
+
+def generate_star (arg):
+    output_file.write(f"star@{arg}|")
+
+
+
+def generate_hoenn (arg):
+    output_file.write(f"hoenn@{arg}|")
+
+
+
+def generate_horn (arg):
+    output_file.write(f"horn@{arg}|")
+
+
+
+def generate_gaster (arg):
+    output_file.write(f"gaster@{arg}|")
+
+
+
+def generate_toby (arg):
+    output_file.write(f"toby@{arg}|")
+
+
+
+def generate_undertale_crack (arg):
+    output_file.write(f"undertale_crack@{arg}|")
+
+
+
+def generate_undertale_hit (arg):
+    output_file.write(f"undertale_hit@{arg}|")
+
+
+
+def generate_undertale_encounter (arg):
+    output_file.write(f"undertale_encounter@{arg}|")
+
+
+
+def generate_megalovania (arg):
+    output_file.write(f"megalovania@{arg}|")
+
+
+
+def generate_fnf_death (arg):
+    output_file.write(f"fnf_death@{arg}|")
+
+
+
+def generate_fnf_right (arg):
+    output_file.write(f"fnf_right@{arg}|")
+
+
+
+def generate_fnf_up (arg):
+    output_file.write(f"fnf_up@{arg}|")
+
+
+
+def generate_fnf_down (arg):
+    output_file.write(f"fnf_down@{arg}|")
+
+
+
+def generate_fnf_left (arg):
+    output_file.write(f"fnf_left@{arg}|")
+
+
+
+def generate_gdcrash (arg):
+    output_file.write(f"gdcrash@{arg}|")
+
+
+
+def generate_gdcrash_orbs (arg):
+    output_file.write(f"gdcrash_orbs@{arg}|")
+
+
+
+def generate_gd_coin (arg):
+    output_file.write(f"gd_coin@{arg}|")
+
+
+
+def generate_gd_orbs (arg):
+    output_file.write(f"gd_orbs@{arg}|")
+
+
+
+def generate_gd_diamonds (arg):
+    output_file.write(f"gd_diamonds@{arg}|")
+
+
+
+def generate_bwomp (arg):
+    output_file.write(f"bwomp@{arg}|")
+
+
+
+def generate_isaac_hurt (arg):
+    output_file.write(f"isaac_hurt@{arg}|")
+
+
+
+def generate_isaac_dead (arg):
+    output_file.write(f"isaac_dead@{arg}|")
+
+
+
+def generate_isaac_mantle (arg):
+    output_file.write(f"isaac_mantle@{arg}|")
+
+
+
+def generate_terraria_star (arg):
+    output_file.write(f"terraria_star@{arg}|")
+
+
+
+def generate_terraria_pot (arg):
+    output_file.write(f"terraria_pot@{arg}|")
+
+
+
+def generate_terraria_reforge (arg):
+    output_file.write(f"terraria_reforge@{arg}|")
+
+
+
+def generate_amogus (arg):
+    output_file.write(f"amogus@{arg}|")
+
+
+
+def generate_amongdrip (arg):
+    output_file.write(f"amongdrip@{arg}|")
+
+
+
+def generate_amongus (arg):
+    output_file.write(f"amongus@{arg}|")
+
+
+
+def generate_flipnote (arg):
+    output_file.write(f"flipnote@{arg}|")
+
+
+
+def generate_ultrainstinct (arg):
+    output_file.write(f"ultrainstinct@{arg}|")
+
+
+
+def generate_celeste_diamond (arg):
+    output_file.write(f"celeste_diamond@{arg}|")
+
+
+
+def generate_celeste_death (arg):
+    output_file.write(f"celeste_death@{arg}|")
+
+
+
+def generate_celeste_spring (arg):
+    output_file.write(f"celeste_spring@{arg}|")
+
+
+
+def generate_celeste_dash (arg):
+    output_file.write(f"celeste_dash@{arg}|")
+
+
+
+def generate_DEFEAT (arg):
+    output_file.write(f"DEFEAT@{arg}|")
+
+
+
+def generate_YOU (arg):
+    output_file.write(f"YOU@{arg}|")
+
+
+
+def generate_BABA (arg):
+    output_file.write(f"BABA@{arg}|")
+
+
+
+def generate_noteblock_harp (arg):
+    output_file.write(f"noteblock_harp@{arg}|")
+
+
+
+def generate_noteblock_bass (arg):
+    output_file.write(f"noteblock_bass@{arg}|")
+
+
+
+def generate_noteblock_snare (arg):
+    output_file.write(f"noteblock_snare@{arg}|")
+
+
+
+def generate_noteblock_click (arg):
+    output_file.write(f"noteblock_click@{arg}|")
+
+
+
+def generate_noteblock_bell (arg):
+    output_file.write(f"noteblock_bell@{arg}|")
+
+
+
+def generate_noteblock_chime (arg):
+    output_file.write(f"noteblock_chime@{arg}|")
+
+
+
+def generate_noteblock_banjo (arg):
+    output_file.write(f"noteblock_banjo@{arg}|")
+
+
+
+def generate_noteblock_pling (arg):
+    output_file.write(f"noteblock_pling@{arg}|")
+
+
+
+def generate_noteblock_xylophone (arg):
+    output_file.write(f"noteblock_xylophone@{arg}|")
+
+
+
+def generate_noteblock_bit (arg):
+    output_file.write(f"noteblock_bit@{arg}|")
+
+
+
+def generate_minecraft_explosion (arg):
+    output_file.write(f"minecraft_explosion@{arg}|")
+
+
+
+def generate_minecraft_bell (arg):
+    output_file.write(f"minecraft_bell@{arg}|")
+
+
+
+def generate_boom_system (arg):
+    output_file.write(f"!boom@{arg}|")
+
